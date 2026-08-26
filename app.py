e4fe6e3b2c1883c116a96e655f4ee7d8613792a0
Girish2229 <2420030031@klh.edu.in>
2026-08-26 12:39:35 +0530

    Add login feature

---
+++ app.py
@@ -0,0 +1,11 @@
+def login(username):
+    return f"Welcome {username}"
+
+print(login("Student"))
+
+
+cat > app.py <<'EOF'
+def login(username):
+    return f"Welcome {username}"
+
+print(login("Student"))
